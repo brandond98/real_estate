d7320f1bdfa572297ec3b1de114a6d1b32744af4
Brandon Dickson <bdickson1212@gmail.com>
2019-01-15 00:00:55 +0000

    register page now works just needs error messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 def register(request):
     if request.method == 'POST':
         #Get form values
         first_name = request.POST['first_name']
-        first_name = request.POST['last_name']
-        first_name = request.POST['username']
-        first_name = request.POST['email']
-        first_name = request.POST['password']
-        first_name = request.POST['password2']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check username
+        if User.objects.filter(username=username).exists():
+            print('username taken')
+            return redirect('register')
+        else:
+            if User.objects.filter(email=email).exists():
+                print('email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name,)
+                auth.login(request, user)
+                return redirect('index')
     else:
         return render(request, 'apps/accounts/register.html')
 
